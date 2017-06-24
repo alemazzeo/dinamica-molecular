2123fcfb0074d3643dac78ec1a61b77cb2764e49
micadeza <miica.vl@hotmail.com>
2017-06-24 18:19:09 -0300

    agrego funcion lambda pero anda mal

--- src/md.c
+++ src/md.c
@@ -8,7 +8,7 @@
 #include "energia.h"
 
 int main(int argc, char **argv){
-    int N = 10; // Nr de particulas
+    int N = 2; // Nr de particulas
     float rho = 0.8442; //Densidad
     float L = N/rho; // Longitud de la caja (el doble de rc)
     float rc = 0.5*L; // Maxima influencia del potencial
@@ -21,7 +21,7 @@ int main(int argc, char **argv){
     float *vel = malloc(3*N*sizeof(float));
     float *fza = malloc(3*N*sizeof(float));
     float *fza_aux = malloc(3*N*sizeof(float));
-
+    float *lambda = malloc(niter*sizeof(float));
     // Inicializa la caja con las N partiuclas
     llenar(pos, N, L);
     velocidades(vel, N, T);
@@ -29,11 +29,17 @@ int main(int argc, char **argv){
     for(i=0;i<niter;i++){
         verlet(pos, vel, &fza, &fza_aux, N, L, h, rc);
         printf("%f\t%f\t%f\n", pos[0],pos[1],pos[2]);
+        lambda[i] = lambda_verlet (pos, N, L);
+    }
+
+    for (i=0;i<niter;i++) {
+        printf("%f\n", lambda[i]);
     }
 
     free(pos);
     free(vel);
     free(fza);
     free(fza_aux);
+    free(lambda);
 
 }
