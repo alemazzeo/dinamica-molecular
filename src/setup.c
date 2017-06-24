b618f6596cd8dd12d2d1474aa54a1312e4d6e775
micadeza <miica.vl@hotmail.com>
2017-06-24 14:33:41 -0300

    detalles de codigo

--- src/setup.c
+++ src/setup.c
@@ -6,81 +6,76 @@
 #include "setup.h"
 
 int llenar (float *pos, float N, float L){
-  int n,i,j,k,l,s;
-  float p,a;
-  p = pow(N, (double)1/3);
-  n = floor(p);
-  //printf("%d\n", n );
-  a = L/N;
-  //printf("%f\n", a);
-  float r = p - n;
-  if (r < 0.5) {
-    n = n;
-    s = N - n*n*n;
-  //printf("%d\n", s);
-  for (k=0;k<n;k++){
-    for (j=0;j<n;j++){
-      for (i=0;i<n;i++){
-        *(pos + 3*i + (3*n)*j + (3*n*n)*k) = (a/2) + i*a;
-        *(pos + 1 + 3*i + (3*n)*j + (3*n*n)*k) = (a/2) + j*a;
-        *(pos + 2 + 3*i + (3*n)*j + (3*n*n)*k) = (a/2) + k*a;
-      }
-    }
-  }
+    int n,i,j,k,s;
+    float p,a;
+    p = pow(N, (double)1/3); //raiz cubica del numero de particulas
+    n = floor(p); // parte entera de p.
+    a = L/N; // Distancia entre particulas
+    float r = p - n; // r es la parte decimal de p
+    // si r < 0.5 acomodo las particulas en la caja y las que sobra las distribuyo después.
+    if (r < 0.5) {
+        n = n;
+        s = N - n*n*n; //cantidad de particulas que sobran
+  //Acomodo las particulas. El for en i es para la coordenada x, el de j para y y el de k para z.
+        for (k=0;k<n;k++){
+            for (j=0;j<n;j++){
+                for (i=0;i<n;i++){
+                    pos [3*i + (3*n)*j + (3*n*n)*k] = (a/2) + i*a;
+                    pos [1 + 3*i + (3*n)*j + (3*n*n)*k] = (a/2) + j*a;
+                    pos [2 + 3*i + (3*n)*j + (3*n*n)*k] = (a/2) + k*a;
+                }
+            }
+        }
 
-  if (s<=n){
-    for (i=0;i<s;i++) {
-    *(pos + 3*(n*n*n) + 3*i) = a + i*a;
-    *(pos + 1 + 3*(n*n*n) + 3*i) = a;
-    *(pos + 2 + 3*(n*n*n) + 3*i) = a ;
-  }
-}
- int v = floor (s/n);
-   if (s>n && s<n*n){
-    for (j=0;j<v;j++) {
-      for (i=0;i<n;i++) {
-     *(pos + 3*(n*n*n) + 3*i + 3*n*j) = a + i*a;
-     *(pos + 1 + 3*(n*n*n) + 3*i + 3*n*j) = a + j*a;
-     *(pos + 2 + 3*(n*n*n) + 3*i + 3*n*j) = a ;
-   }
- }
- }
- int vv = floor (s/n*n);
+  //Veo los casos de las particulas que sobran
+  // s<=n: Entran en un eje
+        if (s<=n) {
+            for (i=0;i<s;i++) {
+                pos [3*(n*n*n) + 3*i] = a + i*a;
+                pos [1 + 3*(n*n*n) + 3*i] = a;
+                pos [2 + 3*(n*n*n) + 3*i] = a ;
+            }
+        }
 
-if (s>n*n && s<n*n*n) {
-  for (k=0;k<vv;k++){
-  for (j=0;j<v;j++) {
-    for (i=0;i<n;i++) {
-      *(pos + 3*(n*n*n) + 3*i + 3*n*j + 3*(n*n)*k) = a + i*a;
-      *(pos + 1 + 3*(n*n*n) + 3*i + 3*n*j + 3*(n*n)*k) = a + j*a;
-      *(pos + 2 + 3*(n*n*n) + 3*i + 3*n*j + 3*(n*n)*k) = a + k*a ;
-    }
-  }
+//s>n y s<n*n entran en un plano intercaladas entre las demas particulas.
+        int v = floor (s/n); //límite para j
+        if (s>n && s<n*n) {
+            for (j=0;j<v;j++) {
+                for (i=0;i<n;i++) {
+                    pos [3*(n*n*n) + 3*i + 3*n*j] = a + i*a;
+                    pos [1 + 3*(n*n*n) + 3*i + 3*n*j] = a + j*a;
+                    pos [2 + 3*(n*n*n) + 3*i + 3*n*j] = a ;
+                }
+            }
+        }
+        int vv = floor (s/n*n); //límite para k
+         // s>n*n y s<n*n*n las voy acomodando en distintos planos.
+        if (s>n*n && s<n*n*n) {
+            for (k=0;k<vv;k++){
+                for (j=0;j<v;j++) {
+                    for (i=0;i<n;i++) {
+                        pos [3*(n*n*n) + 3*i + 3*n*j + 3*(n*n)*k] = a + i*a;
+                        pos [1 + 3*(n*n*n) + 3*i + 3*n*j + 3*(n*n)*k] = a + j*a;
+                        pos [2 + 3*(n*n*n) + 3*i + 3*n*j + 3*(n*n)*k] = a + k*a ;
+                    }
+                }
+            }
+        }
+    }  else {
+        // si r>0.5 pongo mas sobre cada eje, n+1 y que queden libres los últimos espacios.
+        n = n+1;
+        for (k=0;k<n;k++){
+            for (j=0;j<n;j++){
+                for (i=0;i<n;i++) {
+                    pos [3*i + (3*n)*j + (3*n*n)*k] = (a/2) + i*a;
+                    pos [1 + 3*i + (3*n)*j + (3*n*n)*k] = (a/2) + j*a;
+                    pos [2 + 3*i + (3*n)*j + (3*n*n)*k] = (a/2) + k*a;
+                }
+            }
+        }
 }
-}
-}
-  else {
-    n = n+1;
 
-  printf("%d\n", n );
-  for (k=0;k<n;k++){
-    for (j=0;j<n;j++){
-      for (i=0;i<n;i++) {
-      *(pos + 3*i + (3*n)*j + (3*n*n)*k) = (a/2) + i*a;
-      *(pos + 1 + 3*i + (3*n)*j + (3*n*n)*k) = (a/2) + j*a;
-      *(pos + 2 + 3*i + (3*n)*j + (3*n*n)*k) = (a/2) + k*a;
-      }
-    }
-  }
-}
-
-/*
-  for (l=0;l<3*N;l++) {
-    printf ("%f\n",*(pos + l));
-  }
- */
-
-return 0;
+    return 0;
 
 }
 
