cb8a073090784f6b81c4e77b190bb524dd2404bf
Mazzeo Alejandro <ale.exactas@gmail.com>
2017-07-17 03:09:21 -0300

    Nuevos graficos para 512 particulas en datos/corrida2/n512/figuras

--- src/md_ej2.py
+++ src/md_ej2.py
@@ -40,6 +40,9 @@ ruta = args_params.ruta
 # Figura (tamaño)
 plt.rc('figure', figsize=(8, 6))
 
+# Figura (subplot)
+plt.rc('figure.subplot', left=0.15)
+
 # Ticks (tamaño de la fuente)
 plt.rc(('xtick', 'ytick'), labelsize=14)
 
@@ -66,6 +69,8 @@ plt.rc('axes', prop_cycle=lc_cycler * lw_cycler * ls_cycler)
 ####################
 
 archivos = os.listdir(ruta)
+
+N = int(ruta.split('/')[-2][1:])
 
 configs = [[float(a.split('_')[1]), np.load(ruta + a)] for a in archivos
            if a.endswith('config.npy')]
@@ -94,6 +99,7 @@ presion = []
 std_presion = []
 ld_avg = []
 ld_std = []
+volumen = []
 
 for r, dato in datos:
     rho.append(r)
@@ -103,7 +109,156 @@ for r, dato in datos:
     std_temp.append(dato[3])
     energia.append(dato[4])
     std_energia.append(dato[5])
-    presion.append(dato[6])
-    std_presion.append(dato[7])
+
+    vol = N / r
+    presion_corregida = (dato[6] / vol + 1) * r * dato[2]
+    std_corregida = dato[7] * r * dato[2] / vol
+
+    presion.append(presion_corregida)
+    std_presion.append(std_corregida)
     ld_avg.append(dato[8])
     ld_std.append(dato[9])
+    volumen.append(np.repeat(vol, len(dato[0])))
+
+##########################
+# FUNCIONES PARA GRAFICAR
+##########################
+
+
+def plot_temperatura(i=0, ax=None, errorbar=True):
+    str_rho = '%3.1f' % rho[i]
+    if ax is None:
+        fig, ax = plt.subplots(1)
+
+    if errorbar:
+        ax.errorbar(array_pasos[i], temp_real[i], yerr=std_temp[i],
+                    elinewidth=0.5, ls=' ', marker='o', alpha=0.5,
+                    label=r'Temperatura (<v²>) ($\rho$ ' + str_rho + ')')
+    else:
+        ax.plot(array_pasos[i], temp_real[i], marker='o',
+                label=r'Temperatura (<v²>) ($\rho$ ' + str_rho + ')')
+
+    ax.plot(array_pasos[i], temp[i], ls='--', label='Temperatura buscada')
+    ax.set_xlabel('Muestras')
+    ax.set_ylabel(r'Temperatura')
+    ax.legend(loc='best')
+
+
+def plot_energia(i=0, ax=None, errorbar=True):
+    str_rho = '%3.1f' % rho[i]
+    if ax is None:
+        fig, ax = plt.subplots(1)
+
+    if errorbar:
+        ax.errorbar(temp_real[i], energia[i],
+                    xerr=std_temp[i], yerr=std_energia[i],
+                    label=r'Energía ($\rho$: ' + str_rho + ')',
+                    ls=' ', marker='o', elinewidth=0.5)
+    else:
+        ax.plot(temp_real[i], energia[i], label='Energía')
+
+    ax.set_xlabel('Temperatura')
+    ax.set_ylabel(r'Energía')
+    ax.legend(loc='best')
+
+
+def plot_presion(i=0, ax=None, errorbar=True):
+    str_rho = '%3.1f' % rho[i]
+    if ax is None:
+        fig, ax = plt.subplots(1)
+
+    if errorbar:
+        ax.errorbar(temp_real[i], presion[i],
+                    xerr=std_temp[i], yerr=std_presion[i],
+                    label=r'Presión ($\rho$: ' + str_rho + ')',
+                    ls=' ', marker='o', elinewidth=0.5)
+    else:
+        ax.plot(temp_real[i], presion[i], label='Presión')
+    ax.set_xlabel(r'Temperatura')
+    ax.set_ylabel(r'Presión')
+    ax.legend(loc='best')
+
+
+def plot_lindemann(i=0, ax=None, errorbar=True):
+    str_rho = '%3.1f' % rho[i]
+    if ax is None:
+        fig, ax = plt.subplots(1)
+
+    if errorbar:
+        ax.errorbar(energia[i], ld_avg[i],
+                    xerr=std_energia[i], yerr=ld_std[i],
+                    label=r'Coef. de Lindemann ($\rho$: ' + str_rho + ')',
+                    ls=' ', marker='o', elinewidth=0.5)
+    else:
+        ax.errorbar(energia[i], ld_avg[i],
+                    label=r'Coef. de Lindemann ($\rho$: ' + str_rho + ')')
+
+    ax.set_xlabel('Energía')
+    ax.set_ylabel(r'Coef. de Lindemann')
+    ax.legend(loc='best')
+
+
+def plot_lindemann_array(i=0, j=-1, ax=None, errorbar=True):
+    if ax is None:
+        fig, ax = plt.subplots(1)
+
+    x = np.arange(100) * 50
+    y, yerr = np.load(lds_list[i][1][j])
+    str_rho_ld = r'$\rho$ ' + '%-6.2f' % float(lds_list[i][1][j].split('_')[2])
+    str_temp_ld = r'$T=$ ' + '%-6.2f' % float(lds_list[i][1][j].split('_')[4])
+
+    if errorbar:
+        ax.errorbar(x, y, yerr=yerr, marker='.', label='LD - ' + str_temp_ld,
+                    elinewidth=0.5, errorevery=2, ls=' ', color='k')
+    else:
+        ax.plot(x, y, label='LD - ' + str_temp_ld)
+
+    ax.set_xlabel('Pasos')
+    ax.set_ylabel('Coef. de Lindemann (' + str_rho_ld + ')')
+    ax.annotate(str_temp_ld, xy=(x[-1], y[-1]), xytext=(x[-1] + 100, y[-1]),
+                fontsize=14)
+    ax.set_xlim([-50, x[-1] + 1000])
+
+
+t1 = [1.05, 1.20, 0.95, 0.90, 0.80, 0.65, 0.43]
+t1e = [0.05, 0.10, 0.05, 0.05, 0.02, 0.05, 0.01]
+t2 = [0.36, 0.31, 0.33, 0.36, 0.34, 0.33, 0.31]
+t2e = [0.01, 0.01, 0.02, 0.03, 0.01, 0.02, 0.01]
+
+
+###########################
+# GRÁFICOS PARA EL INFORME
+###########################
+
+plt.ioff()
+
+ruta_figuras = ruta + '/figuras/'
+os.makedirs(ruta_figuras, exist_ok=True)
+
+for i, r in enumerate(rho):
+    str_rho = '%3.1f' % r
+    fig, ax = plt.subplots(1)
+    plot_energia(i=i, ax=ax, errorbar=True)
+    fig.savefig(ruta_figuras + 'energia_rho_' + str_rho + '_fig.png')
+    plt.close()
+
+for i, r in enumerate(rho):
+    str_rho = '%3.1f' % r
+    fig, ax = plt.subplots(1)
+    plot_presion(i=i, ax=ax, errorbar=True)
+    fig.savefig(ruta_figuras + 'presion_rho_' + str_rho + '_fig.png')
+    plt.close()
+
+for i, r in enumerate(rho):
+    str_rho = '%3.1f' % r
+    fig, ax = plt.subplots(1)
+    plot_lindemann(i=i, ax=ax, errorbar=True)
+    fig.savefig(ruta_figuras + 'lindemann_rho_' + str_rho + '_fig.png')
+    plt.close()
+
+for i, r in enumerate(rho):
+    str_rho = '%3.1f' % r
+    fig, ax = plt.subplots(1)
+    plot_temperatura(i=i, ax=ax, errorbar=True)
+    fig.savefig(ruta_figuras + 'temp_rho_' + str_rho + '_fig.png')
+    plt.close()
